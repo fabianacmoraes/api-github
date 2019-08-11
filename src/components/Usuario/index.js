1fd93349bd783b53ae348e2ee0c611271602099e
fabianacmoraes <fabianacorream@hotmail.com>
2019-08-11 01:29:02 -0300

    refactor: Adição de validações do atributo user

--- src/components/Usuario/index.js
+++ src/components/Usuario/index.js
@@ -1,56 +1,62 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import './usuario.css'
+import "./usuario.css";
 
 class Usuario extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    };
+  }
 
-    constructor() {
-        super()
-        this.state = {
-            user: {}
+  componentDidMount() {
+    if (
+      this.props.user &&
+      this.props.user.login &&
+      this.props.user.login.length > 0
+    )
+      fetch(`https://api.github.com/users/${this.props.user.login}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
         }
-    }
+      })
+        .then(response => response.json())
+        .then(responseJson => {
+          this.setState({
+            user: responseJson
+          });
+        });
+  }
 
-    componentDidMount() {
-        fetch(`https://api.github.com/users/${this.props.user.login}`,
-            {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-            })
-            .then(response => response.json())
-            .then(responseJson => {
-                console.log(responseJson)
-                this.setState({
-                    user: responseJson,
-                })
-            })
-    }
-
-    render() {
-        return (
-            <article className="usuario">
-                <div className="usuario__cabecalho">
-                    <img className="usuario__fotoUsuario" src={this.state.user.avatar_url} alt="" />
-                    <span className="usuario__nomeUsuario">{this.state.user.login}</span>
-                </div>
-                {/* <p className="usuario__conteudo">{this.state.user.bio}</p>
+  render() {
+    return (
+      <article className="usuario">
+        <div className="usuario__cabecalho">
+          <img
+            className="usuario__fotoUsuario"
+            src={this.state.user.avatar_url}
+            alt=""
+          />
+          <span className="usuario__nomeUsuario">{this.state.user.login}</span>
+        </div>
+        {/* <p className="usuario__conteudo">{this.state.user.bio}</p>
                 <div className="usuario__infos">
                     <p>{this.state.user.followers}</p>
                     <p>{this.state.user.following}</p>
                 </div> */}
-            </article>
-        )
-    }
+      </article>
+    );
+  }
 }
 
 Usuario.propTypes = {
-    user: PropTypes.shape({
-        login: PropTypes.string.isRequired
-    })
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired
+  })
 };
 
-export default Usuario
+export default Usuario;
