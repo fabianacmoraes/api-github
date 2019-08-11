f3216875a095c3a96510b6826bef8d7307d042c3
fabianacmoraes <fabianacorream@hotmail.com>
2019-08-11 01:27:22 -0300

    feat: Criação do componente de listagem de usuários

---
+++ src/components/Listagem/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import Usuario from "../Usuario";
+
+class Listagem extends Component {
+  static getDerivedStateFromProps({ users }, state) {
+    state = { users };
+    return state;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.users && this.state.users.length > 0 ? (
+          this.state.users.map(user => <Usuario user={user} key={user.login} />)
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Listagem;
